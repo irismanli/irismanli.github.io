e116302bd245b47f12f99e7c657bc8f32c22a611
Iris Li <irismanli@yahoo.com>
2025-08-17 16:13:48 -0700

    WIP: okta sample updates.

---
+++ worksamples/Navigate_ITP_Architecture_and_Risk_Models/static/js/public.04648f0b.js
@@ -0,0 +1,12 @@
+"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[624],{87012:()=>{var o="stub_footer__17ec1",s="stub_loader__0d0e3",t="stub_logo__f0e5a",a="stub_main__00e11",l="stub_stub__758ad",d;window.moveTo(0,0),window.resizeTo(screen.availWidth,screen.availHeight);const e=document.getElementById("root");(e==null?void 0:e.innerHTML)===""&&(e.innerHTML=`
+<div class="${l}">
+  <div class="${a}">
+    
+    <img alt="logo" class="${t}" src="${((d=window.defaultData)==null?void 0:d.logo)||window.PLATFORM.logoFull_M}" />
+    <div class="${s}"></div>
+  </div>
+  <div class="footer">
+    ${window.PLATFORM.courseHtmlFooter}
+  </div>
+</div>
+`)}},o=>{var s=a=>o(o.s=a);o.O(0,[646],()=>s(87012));var t=o.O()}]);
